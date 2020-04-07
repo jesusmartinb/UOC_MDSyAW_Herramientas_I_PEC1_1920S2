70127aa464c9e2f4de0a9e01120f8f7c7b5f6651
Jesús Martín Blanco <jmartinblan@uoc.edu>
2020-04-07 23:32:18 +0200

    Se crea slider para el header

--- src/pages/js/index.js
+++ src/pages/js/index.js
@@ -1,18 +1,26 @@
 import '../css/style.css';
 
+import $ from 'jquery';
+
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faTools } from '@fortawesome/free-solid-svg-icons';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { faHtml5 } from '@fortawesome/free-brands-svg-icons';
 import { faCss3Alt } from '@fortawesome/free-brands-svg-icons';
 import { faJsSquare } from '@fortawesome/free-brands-svg-icons';
 
 library.add(faCheck);
+library.add(faArrowUp);
 library.add(faBars);
 library.add(faTools);
 library.add(faHome);
+library.add(faArrowLeft);
+library.add(faArrowRight);
 library.add(faHtml5);
 library.add(faJsSquare);
 library.add(faCss3Alt);
@@ -46,4 +54,90 @@ setTimeout(function () {
     }
 
 },100)
+
+$(document).ready(function () {
+
+    // Mostrar y ocultar el botón "Volver arriba"
+
+    var btnVolverArriba = $('#btnVolverArriba');
+
+    $(window).on('scroll', function () {
+
+        var mainOffsetTop = $('#main').offset().top;
+
+        if ($(window).scrollTop() >= mainOffsetTop) {
+
+            btnVolverArriba.css('margin-right', 0);
+
+        }else if ($(window).scrollTop() <= mainOffsetTop/2){
+
+            btnVolverArriba.css('margin-right', '-60px');
+
+        }
+
+    })
+
+    // Movimiento suave de scroll de "Inicio" y "Volver arriba"
+
+    $('a.volver-arriba').on('click', function (e) {
+        e.preventDefault();
+
+        if($(window).scrollTop() != 0) {
+            $('html, body').stop().animate({scrollTop: 0}, 1000);
+        }
+    })
+
+    // Efecto slider del header
+    // Seleccionando elementos en variables
+    var slider = $('#slider');
+    var btnAnterior = $('#btnAnterior');
+    var btnSiguiente = $('#btnSiguiente');
+
+    // Se pasa el último slide al primer lugar
+    $('#slider .slide:last').insertBefore('#slider .slide:first');
+
+    // con margen negativo se vuelve a mostrar el primer slider
+    slider.css('margin-left', '-100%');
+
+    // Función para que el slide se mueva a la derecha
+    function moverDerecha() {
+        if(!slider.is(':animated')) {
+            slider.animate({
+                marginLeft: '-200%'
+            }, 1000, function () {
+                $('#slider .slide:first').insertAfter('#slider .slide:last');
+                slider.css('margin-left', '-100%');
+                resetInterval();
+            });
+        }
+    }
+
+    btnSiguiente.on('click', moverDerecha);
+
+
+    // Función para que el slide se mueva a la izquierda
+    function moverIzquierda() {
+        if(!slider.is(':animated')) {
+            $('#slider .slide:last').insertBefore('#slider .slide:first');
+            slider.css('margin-left', '-200%');
+            slider.animate({
+                marginLeft: '-100%'
+            }, 1000, function () {
+                resetInterval();
+            });
+        }
+    }
+
+    btnAnterior.on('click', moverIzquierda);
+
+
+    // Intervalo para que el slide se mueva automáticamente
+    var intervalo = setInterval(moverDerecha, 5000);
+
+    function resetInterval() {
+        clearInterval(intervalo);
+        intervalo = setInterval(moverDerecha, 5000);
+    }
+
+});
 
