ca44618397bee1084a62652ae52d5ea54ac3eb92
Jesús Martín Blanco <jmartinblan@uoc.edu>
2020-04-09 11:39:40 +0200

    Se optimizan las imagenes con imagemin

---
+++ imagemin.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	"gifsicle": {
+		"optimizationLevel": 2,
+		"interlaced": false,
+		"colors": 10
+	},
+	"mozjpeg": {
+		"progressive": true,
+		"quality": 30
+	},
+	"pngquant": {
+		"quality": [0.25, 0.5]
+	},
+	"svgo": {
+		"plugins": [
+			{ "removeViewBox": false },
+			{ "cleanupIDs": true },
+		]
+	}
+}
